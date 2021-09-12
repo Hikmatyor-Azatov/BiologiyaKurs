45ea0758e2d8c74bf9fc16da1f55f1eff6562418
Hikmatyor-Azatov <azatovhikmatyor@gmail.com>
2021-09-12 23:08:09 +0500

    Added basic configurations

--- index.js
+++ index.js
@@ -1,53 +1,10 @@
-$(document).ready(function () {
-    let bars = $("#top_bars");
-    let topNav = $(".topNav");
-    let topNavOpen = true;
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
 
-    bars.click(function () {
-        topNav.slideToggle();
-        if (topNavOpen) {
-            bars.removeClass("fa-window-close");
-            bars.addClass("fa-bars");
-            topNavOpen = false;
-        } else {
-            bars.removeClass("fa-bars");
-            bars.addClass("fa-window-close");
-            topNavOpen = true;
-        }
-    });
-});
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-    showSlides((slideIndex += n));
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    captionText.innerHTML = dots[slideIndex - 1].alt;
-}
+express()
+    .use(express.static(path.join(__dirname, "public")))
+    .set("views", path.join(__dirname, "views"))
+    .set("view engine", "ejs")
+    .get("/", (req, res) => res.render("index"))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
